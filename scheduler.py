c12971b226b2fd3b70fa5c170a062b0dd4532283
Kidhar Bachan <kidharb@gmail.com>
2024-06-04 22:02:49 +0200

    Initial 0MQ server

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,13 @@
-placeholder
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.PAIR)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print("Sending request %s …" % request)
+    socket.send(b"Hello")
